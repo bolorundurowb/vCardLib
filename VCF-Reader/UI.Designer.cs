593f6220382f823cc6e08de68519b21b119cef05
bolorundurowb <ogatimo@gmail.com>
2016-04-03 19:56:05 +0100

    Icon and excluded files added

---
+++ VCF-Reader/UI.Designer.cs
@@ -0,0 +1,191 @@
+﻿namespace VCF_Reader
+{
+    partial class UI
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(UI));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lbl_file_path = new System.Windows.Forms.Label();
+            this.txt_search = new System.Windows.Forms.TextBox();
+            this.btn_browse = new System.Windows.Forms.Button();
+            this.dgv_display = new System.Windows.Forms.DataGridView();
+            this.lastName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.firstName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.emailAddress = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.phoneNumber = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.phoneNumber2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ofd_pick_file = new System.Windows.Forms.OpenFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_display)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(74, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "VCF File Path:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 41);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Search:";
+            // 
+            // lbl_file_path
+            // 
+            this.lbl_file_path.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lbl_file_path.Location = new System.Drawing.Point(84, 10);
+            this.lbl_file_path.Name = "lbl_file_path";
+            this.lbl_file_path.Size = new System.Drawing.Size(447, 18);
+            this.lbl_file_path.TabIndex = 2;
+            // 
+            // txt_search
+            // 
+            this.txt_search.Location = new System.Drawing.Point(59, 38);
+            this.txt_search.Name = "txt_search";
+            this.txt_search.Size = new System.Drawing.Size(266, 20);
+            this.txt_search.TabIndex = 3;
+            this.txt_search.TextChanged += new System.EventHandler(this.txt_search_TextChanged);
+            // 
+            // btn_browse
+            // 
+            this.btn_browse.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btn_browse.Location = new System.Drawing.Point(537, 8);
+            this.btn_browse.Name = "btn_browse";
+            this.btn_browse.Size = new System.Drawing.Size(30, 23);
+            this.btn_browse.TabIndex = 4;
+            this.btn_browse.Text = "...";
+            this.btn_browse.UseVisualStyleBackColor = true;
+            this.btn_browse.Click += new System.EventHandler(this.btn_browse_Click);
+            // 
+            // dgv_display
+            // 
+            this.dgv_display.AllowUserToAddRows = false;
+            this.dgv_display.AllowUserToDeleteRows = false;
+            this.dgv_display.AllowUserToOrderColumns = true;
+            this.dgv_display.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dgv_display.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgv_display.BackgroundColor = System.Drawing.SystemColors.Control;
+            this.dgv_display.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv_display.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.lastName,
+            this.firstName,
+            this.emailAddress,
+            this.phoneNumber,
+            this.phoneNumber2});
+            this.dgv_display.Location = new System.Drawing.Point(12, 77);
+            this.dgv_display.Name = "dgv_display";
+            this.dgv_display.ReadOnly = true;
+            this.dgv_display.RowHeadersVisible = false;
+            this.dgv_display.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgv_display.Size = new System.Drawing.Size(555, 268);
+            this.dgv_display.TabIndex = 6;
+            // 
+            // lastName
+            // 
+            this.lastName.HeaderText = "Last Name";
+            this.lastName.Name = "lastName";
+            this.lastName.ReadOnly = true;
+            // 
+            // firstName
+            // 
+            this.firstName.HeaderText = "First Name";
+            this.firstName.Name = "firstName";
+            this.firstName.ReadOnly = true;
+            // 
+            // emailAddress
+            // 
+            this.emailAddress.HeaderText = "Email Address";
+            this.emailAddress.Name = "emailAddress";
+            this.emailAddress.ReadOnly = true;
+            // 
+            // phoneNumber
+            // 
+            this.phoneNumber.HeaderText = "Phone Number";
+            this.phoneNumber.Name = "phoneNumber";
+            this.phoneNumber.ReadOnly = true;
+            // 
+            // phoneNumber2
+            // 
+            this.phoneNumber2.HeaderText = "Phone Number 2";
+            this.phoneNumber2.Name = "phoneNumber2";
+            this.phoneNumber2.ReadOnly = true;
+            // 
+            // ofd_pick_file
+            // 
+            this.ofd_pick_file.Filter = "VCF Files|*.vcf";
+            this.ofd_pick_file.Title = "Pick Contact File";
+            // 
+            // UI
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(579, 357);
+            this.Controls.Add(this.dgv_display);
+            this.Controls.Add(this.btn_browse);
+            this.Controls.Add(this.txt_search);
+            this.Controls.Add(this.lbl_file_path);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "UI";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "VCF Reader 1.0";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.UI_FormClosing);
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_display)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lbl_file_path;
+        private System.Windows.Forms.TextBox txt_search;
+        private System.Windows.Forms.Button btn_browse;
+        private System.Windows.Forms.DataGridView dgv_display;
+        private System.Windows.Forms.OpenFileDialog ofd_pick_file;
+        private System.Windows.Forms.DataGridViewTextBoxColumn lastName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn firstName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn emailAddress;
+        private System.Windows.Forms.DataGridViewTextBoxColumn phoneNumber;
+        private System.Windows.Forms.DataGridViewTextBoxColumn phoneNumber2;
+    }
+}
+
---
+++ VCF-Reader/UI.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+using Thought.vCards;
+
+namespace VCF_Reader
+{
+    public partial class UI : Form
+    {
+        private List<DataGridViewRow> allRows;
+
+        public UI()
+        {
+            InitializeComponent();
+            allRows = new List<DataGridViewRow>();
+            if (!string.IsNullOrWhiteSpace(Properties.Settings.Default.LastFilePath))
+                if (Properties.Settings.Default.LastFilePath.Contains(".vcf"))
+                {
+                    LoadVCF(Properties.Settings.Default.LastFilePath);
+                    lbl_file_path.Text = Properties.Settings.Default.LastFilePath;
+                }
+        }
+
+        private void btn_browse_Click(object sender, EventArgs e)
+        {
+            ofd_pick_file.ShowDialog();
+            if (!string.IsNullOrWhiteSpace(ofd_pick_file.FileName))
+            {
+                lbl_file_path.Text = ofd_pick_file.FileName;
+                LoadVCF(ofd_pick_file.FileName);
+            }
+        }
+
+        private void UI_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (lbl_file_path.Text.Contains(".vcf"))
+            {
+                Properties.Settings.Default.LastFilePath = lbl_file_path.Text;
+                Properties.Settings.Default.Save();
+            }
+        }
+
+        private void LoadVCF(string filePath)
+        {
+            dgv_display.Rows.Clear();
+            vCard vcard = new vCard(filePath);
+            object[] row = new object[5];
+            row[0] = vcard.FamilyName;
+            row[1] = vcard.GivenName;
+            row[2] = vcard.EmailAddresses.Count > 0 ? vcard.EmailAddresses[0].ToString() : "";
+            row[3] = vcard.Phones.Count() > 0 ? vcard.Phones[0].FullNumber : "";
+            row[4] = vcard.Phones.Count() > 1 ? vcard.Phones[1].FullNumber : "";
+            dgv_display.Rows.Add(row);
+
+            foreach (DataGridViewRow dRow in dgv_display.Rows)
+            {
+                allRows.Add(dRow);
+            }
+        }
+
+        private void txt_search_TextChanged(object sender, EventArgs e)
+        {
+            List<DataGridViewRow> matchedRows = new List<DataGridViewRow>();
+            foreach (var row in allRows)
+            {
+                foreach (DataGridViewCell cell in row.Cells)
+                {
+                    if (cell.Value.ToString().Contains(txt_search.Text))
+                        matchedRows.Add(row);
+                }
+            }
+            dgv_display.Rows.Clear();
+            foreach(var row in matchedRows)
+            {
+                if (!dgv_display.Rows.Contains(row))
+                    dgv_display.Rows.Add(row);
+            }
+        }
+    }
+}
