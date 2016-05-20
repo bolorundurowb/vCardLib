a79c04dfbbc387e929a3708dac65e486ee234967
Bolorunduro Winner-Timothy <ogatimo@gmail.com>
2016-05-20 09:55:06 +0100

    Added a missing source file

---
+++ vCardLib/Enums.cs
@@ -0,0 +1,19 @@
+﻿namespace vCardLib
+{
+    public enum GenderType
+    {
+        Male,
+        Female,
+        Other
+    }
+
+    public enum ContactType
+    {
+        Individual,
+        Group,
+        Organization,
+        Location,
+        Application,
+        Device
+    }
+}
