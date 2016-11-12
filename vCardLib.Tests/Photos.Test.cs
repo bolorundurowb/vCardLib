e28a1c6f8f0164c1978a0af5f14ec90f19eef93f
Bolorunduro Winner-Timothy <ogatimo@gmail.com>
2016-11-13 00:15:25 +0100

    test: adds tests for the photos class

---
+++ vCardLib.Tests/Photos.Test.cs
@@ -0,0 +1,32 @@
+﻿using NUnit.Framework;
+
+namespace vCardLib.Tests
+{
+	[TestFixture]
+	public class Photos
+	{
+		[Test]
+		public void WhenPictureIsNull()
+		{
+			Photo photo = new Photo();
+			photo.Type = PhotoType.URL;
+			photo.Encoding = PhotoEncoding.GIF;
+			photo.PhotoURL = "http://google.com/test.gif";
+			photo.Picture = null;
+
+			Assert.AreEqual("", photo.ToBase64String());
+		}
+
+		[Test]
+		public void WhenPictureIsNotNull()
+		{
+			Photo photo = new Photo();
+			photo.Type = PhotoType.Image;
+			photo.Encoding = PhotoEncoding.JPEG;
+			photo.Picture = new System.Drawing.Bitmap("lib-icon.ico");
+
+			Assert.DoesNotThrow(delegate { photo.ToBase64String(); });
+			Assert.Greater(photo.ToBase64String().Length, 0);
+		}
+	}
+}
