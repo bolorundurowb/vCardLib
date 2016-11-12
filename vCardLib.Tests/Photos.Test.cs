a0756b79d081a554e458c8c8fef725d7ebc1964b
Bolorunduro Winner-Timothy <ogatimo@gmail.com>
2016-11-13 00:27:00 +0100

    test: fixes second photos test

--- vCardLib.Tests/Photos.Test.cs
+++ vCardLib.Tests/Photos.Test.cs
@@ -20,10 +20,15 @@ namespace vCardLib.Tests
 		[Test]
 		public void WhenPictureIsNotNull()
 		{
-			Photo photo = new Photo();
+			var request = System.Net.WebRequest.Create("https://jpeg.org/images/jpeg-logo-plain.png");
+            System.Net.WebResponse response = request.GetResponse();
+            System.IO.Stream responseStream =
+                response.GetResponseStream();
+
+            Photo photo = new Photo();
 			photo.Type = PhotoType.Image;
 			photo.Encoding = PhotoEncoding.JPEG;
-			photo.Picture = new System.Drawing.Bitmap("lib-icon.ico");
+			photo.Picture = new System.Drawing.Bitmap(responseStream);
 
 			Assert.DoesNotThrow(delegate { photo.ToBase64String(); });
 			Assert.Greater(photo.ToBase64String().Length, 0);
