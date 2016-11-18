7fc909d31b1ec0f4fa2a54d121681eb5227a9eed
bolorundurowb <ogatimo@gmail.com>
2016-11-18 08:30:08 +0100

    test: adds tests for address and addresscollection

--- vCardLib.Tests/PhotoTests.cs
+++ vCardLib.Tests/PhotoTests.cs
@@ -20,10 +20,14 @@ namespace vCardLib.Tests
 		[Test]
 		public void WhenPictureIsNotNull()
 		{
+			var request = System.Net.WebRequest.Create("https://jpeg.org/images/jpeg-logo-plain.png");
+            System.Net.WebResponse response = request.GetResponse();
+            System.IO.Stream responseStream = response.GetResponseStream();
+
             Photo photo = new Photo();
 			photo.Type = PhotoType.Image;
 			photo.Encoding = PhotoEncoding.JPEG;
-			photo.Picture = new System.Drawing.Bitmap("image.png");
+			photo.Picture = new System.Drawing.Bitmap(responseStream);
 
 			Assert.DoesNotThrow(delegate { photo.ToBase64String(); });
 			Assert.Greater(photo.ToBase64String().Length, 0);
