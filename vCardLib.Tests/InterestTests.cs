8b8b94fc6870e73f7c4cdaabed405b1386f678b2
bolorundurowb <ogatimo@gmail.com>
2016-11-22 05:58:23 +0100

    test: adds tests for multiple files and collections

---
+++ vCardLib.Tests/InterestTests.cs
@@ -0,0 +1,22 @@
+﻿// Created by Bolorunduro Winner-Timothy on  11/22/2016 at 5:33 AM
+using NUnit.Framework;
+
+namespace vCardLib.Tests
+{
+	[TestFixture]
+	public class InterestTests
+	{
+		[Test]
+		public void InterestIsStable()
+		{
+			Assert.DoesNotThrow(delegate
+			{
+				Interest interest = new Interest();
+				interest.Activity = "Tennis";
+				interest.Level = Level.High;
+				interest.Level = Level.Medium;
+				interest.Level = Level.Low;
+			});
+		}
+	}
+}
