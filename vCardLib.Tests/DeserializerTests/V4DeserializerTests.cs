01a849594e553d1086127aaffba7e087e58c29b3
Bolorunduro Winner-Timothy Bamidele <ogatimo@gmail.com>
2017-03-05 16:33:09 +0100

    test: adds tests for V4 serialization and deserialization

--- vCardLib.Tests/DeserializerTests/V4DeserializerTests.cs
+++ vCardLib.Tests/DeserializerTests/V4DeserializerTests.cs
@@ -1,10 +1,16 @@
-﻿using NUnit.Framework;
+﻿using System;
+using NUnit.Framework;
+using vCardLib.Deserializers;
 
 namespace vCardLib.Tests.DeserializerTests
 {
     [TestFixture]
     public class V4DeserializerTests
     {
-
+        [Test]
+        public void ParseTest()
+        {
+            Assert.Throws<NotImplementedException>(delegate { V4Deserializer.Parse(new[] {"test"}); });
+        }
     }
 }
