ef188593bdd404991b281cfd8ca33cc63c8c58e0
Bolorunduro Winner-Timothy Bamidele <ogatimo@gmail.com>
2017-03-08 12:28:14 +0100

    test: adds a test for the from stream reader method

--- vCardLib.Tests/DeserializerTests/DeserializerTests.cs
+++ vCardLib.Tests/DeserializerTests/DeserializerTests.cs
@@ -10,15 +10,34 @@ namespace vCardLib.Tests.DeserializerTests
     [TestFixture]
     public class DeserializerTests
     {
+        string assemblyFolder = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+
         [Test]
         public void FromFileTest()
         {
-            string assemblyFolder = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
             string filePath = Path.Combine(assemblyFolder, "v2.vcf");
             vCardCollection collection = null;
             Assert.DoesNotThrow(delegate
             {
                 collection = Deserializer.FromFile(filePath);
+            });
+            Assert.AreEqual(1, collection.Count);
+        }
+
+        [Test]
+        public void FromStreamReaderTest()
+        {
+            string filePath = Path.Combine(assemblyFolder, "v2.vcf");
+            StreamReader streamReader = null;
+            Assert.DoesNotThrow(delegate
+            {
+                streamReader = Helper.GetStreamReaderFromFile(filePath);
+            });
+            Assert.IsNotNull(streamReader);
+            vCardCollection collection = null;
+            Assert.DoesNotThrow(delegate
+            {
+                collection = Deserializer.FromStreamReader(streamReader);
             });
             Assert.AreEqual(1, collection.Count);
         }
