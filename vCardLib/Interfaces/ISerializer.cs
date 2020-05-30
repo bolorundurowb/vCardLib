b85b382203042826e02b22ebb0b419fd6da64ca6
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2020-05-30 22:26:33 +0100

    ft: add the serializer interface scaffold

---
+++ vCardLib/Interfaces/ISerializer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using vCardLib.Models;
+
+namespace vCardLib.Interfaces
+{
+    public interface ISerializer
+    {
+        string AddPhoneNumbers(IEnumerable<PhoneNumber> phoneNumbers);
+
+        string AddEmailAddresses(IEnumerable<EmailAddress> emailAddresses);
+
+        string AddAddresses(IEnumerable<Address> addresses);
+
+        string AddPhotos(IEnumerable<Photo> photos);
+
+        string AddExpertises(IEnumerable<Expertise> expertises);
+
+        string AddHobbies(IEnumerable<Hobby> hobbies);
+
+        string AddInterests(IEnumerable<Interest> interests);
+    }
+}
