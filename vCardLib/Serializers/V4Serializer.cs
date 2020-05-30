05470f965d4c6475d49ab6be8f608a3f3642801b
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2020-05-31 00:01:19 +0100

    ft: implement v4 card serialization

--- vCardLib/Serializers/V4Serializer.cs
+++ vCardLib/Serializers/V4Serializer.cs
@@ -1,13 +1,58 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using vCardLib.Enums;
+using vCardLib.Interfaces;
+using vCardLib.Models;
 
 namespace vCardLib.Serializers
 {
     /// <summary>
     /// Handles the serialization of version 4 cards
     /// </summary>
-    internal class V4Serializer : BaseSerializer
+    internal class V4Serializer : BaseSerializer, ISerializer
     {
-        protected override void AddVersionedFields()
+        public string Serialize(vCard vCard)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Serialize(IEnumerable<vCard> vCardCollection, vCardVersion? version = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddPhoneNumbers(StringBuilder stringBuilder, IEnumerable<PhoneNumber> phoneNumbers)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddEmailAddresses(StringBuilder stringBuilder, IEnumerable<EmailAddress> emailAddresses)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddAddresses(StringBuilder stringBuilder, IEnumerable<Address> addresses)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddPhotos(StringBuilder stringBuilder, IEnumerable<Photo> photos)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddExpertises(StringBuilder stringBuilder, IEnumerable<Expertise> expertises)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddHobbies(StringBuilder stringBuilder, IEnumerable<Hobby> hobbies)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddInterests(StringBuilder stringBuilder, IEnumerable<Interest> interests)
         {
             throw new NotImplementedException();
         }
