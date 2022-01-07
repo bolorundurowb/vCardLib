6dc33c6991802e7e7361600566590136f0680a67
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2022-01-07 22:16:15 +0100

    ft: add formatted name deserializer

---
+++ vCardLib/Deserialization/FieldDeserializers/FormattedNameDeserializer.cs
@@ -0,0 +1,15 @@
+﻿using vCardLib.Deserialization.Interfaces;
+
+namespace vCardLib.Deserialization.FieldDeserializers
+{
+    internal class FormattedNameDeserializer : IFieldDeserializer<string>
+    {
+        public string FieldKey => "FN";
+        
+        public string Read(string input)
+        {
+            var separatorIndex = input.IndexOf(':');
+            return input.Substring(separatorIndex + 1).Trim();
+        }
+    }
+}
