75e778757487ae540030b72fd082c87df28d44bc
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2022-01-10 19:14:47 +0100

    ft: add lang details scaffold

---
+++ vCardLib/Deserialization/FieldDeserializers/LanguageFieldDeserializer.cs
@@ -0,0 +1,20 @@
+﻿using vCardLib.Deserialization.Interfaces;
+using vCardLib.Models;
+
+namespace vCardLib.Deserialization.FieldDeserializers;
+
+internal class LanguageFieldDeserializer : IFieldDeserializer, IV2FieldDeserializer<Language>, IV3FieldDeserializer<Language>,
+    IV4FieldDeserializer<Language>
+{
+    public string FieldKey => "LANG";
+
+    public Language Read(string input)
+    {
+        var index = input.IndexOf(':');
+        var values = input.Substring(index + 1).Trim();
+        var parts = values.Split(',');
+        var latitude = float.Parse(parts[0]);
+        var longitude = float.Parse(parts[1]);
+        return new Geo(latitude, longitude);
+    }
+}
---
+++ vCardLib/Models/Language.cs
@@ -0,0 +1,10 @@
+﻿namespace vCardLib.Models;
+
+public struct Language
+{
+    public string Type { get; set; }
+    
+    public int? Preference { get; set; }
+    
+    public string Locale { get; set; }
+}
