31307e66964209cd8de5e240f9569b3e3bd13da6
Bolorunduro Winner-Timothy <ogatimo@gmail.com>
2022-01-11 09:39:29 +0000

    ft: add language deserializer support

--- vCardLib/Deserialization/FieldDeserializers/LanguageFieldDeserializer.cs
+++ vCardLib/Deserialization/FieldDeserializers/LanguageFieldDeserializer.cs
@@ -1,20 +1,49 @@
-﻿using vCardLib.Deserialization.Interfaces;
+﻿using System;
+using System.Linq;
+using vCardLib.Deserialization.Interfaces;
 using vCardLib.Models;
 
 namespace vCardLib.Deserialization.FieldDeserializers;
 
-internal class LanguageFieldDeserializer : IFieldDeserializer, IV2FieldDeserializer<Language>, IV3FieldDeserializer<Language>,
-    IV4FieldDeserializer<Language>
+internal class LanguageFieldDeserializer : IFieldDeserializer, IV2FieldDeserializer<Language?>,
+    IV3FieldDeserializer<Language?>,
+    IV4FieldDeserializer<Language?>
 {
     public string FieldKey => "LANG";
 
-    public Language Read(string input)
+    public Language? Read(string input)
     {
-        var index = input.IndexOf(':');
-        var values = input.Substring(index + 1).Trim();
-        var parts = values.Split(',');
-        var latitude = float.Parse(parts[0]);
-        var longitude = float.Parse(parts[1]);
-        return new Geo(latitude, longitude);
+        var parts = input.Split(':');
+
+        // the lang entry must have two parts
+        if (parts.Length != 2)
+            return null;
+
+        int? preference = null;
+        string? type = null;
+
+        var metadata = parts[0].Split(';');
+
+        var typeMetadatum = metadata.First(x => x.StartsWith("TYPE", StringComparison.CurrentCultureIgnoreCase));
+
+        if (!string.IsNullOrWhiteSpace(typeMetadatum))
+        {
+            var typeParts = typeMetadatum.Split('=');
+
+            if (typeParts.Length == 2)
+                type = typeParts[1];
+        }
+
+        var preferenceMetadatum = metadata.First(x => x.StartsWith("PREF", StringComparison.CurrentCultureIgnoreCase));
+
+        if (!string.IsNullOrWhiteSpace(preferenceMetadatum))
+        {
+            var prefParts = preferenceMetadatum.Split('=');
+
+            if (prefParts.Length == 2 && int.TryParse(prefParts[1], out var pref))
+                preference = pref;
+        }
+
+        return new Language(parts[1], preference, type);
     }
 }
--- vCardLib/Models/Language.cs
+++ vCardLib/Models/Language.cs
@@ -3,8 +3,15 @@
 public struct Language
 {
     public string Type { get; set; }
-    
+
     public int? Preference { get; set; }
-    
-    public string Locale { get; set; }
+
+    public string? Locale { get; set; }
+
+    public Language(string type, int? preference, string? locale)
+    {
+        Type = type;
+        Preference = preference;
+        Locale = locale;
+    }
 }
