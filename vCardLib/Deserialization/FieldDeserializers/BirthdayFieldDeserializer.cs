4636d1f709572789c86082020a1902485e65333f
Bolorunduro Winner-Timothy <ogatimo@gmail.com>
2022-01-11 19:34:27 +0000

    ft: add in birthday field deserializer

---
+++ vCardLib/Deserialization/FieldDeserializers/BirthdayFieldDeserializer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Globalization;
+using vCardLib.Deserialization.Interfaces;
+
+namespace vCardLib.Deserialization.FieldDeserializers;
+
+internal class BirthdayFieldDeserializer : IFieldDeserializer, IV2FieldDeserializer<DateTime?>,
+    IV3FieldDeserializer<DateTime?>, IV4FieldDeserializer<DateTime?>
+{
+    public string FieldKey => "BDAY";
+
+    public DateTime? Read(string input)
+    {
+        IFormatProvider provider = new CultureInfo("en-US");
+        const string dateFormat = "yyyyMMdd";
+        const string timeFormat = "--HHmm";
+        const string dateTimeFormat = "yyyyMMddTHHmmssZ";
+
+        input = input.Replace(FieldKey, string.Empty, StringComparison.CurrentCultureIgnoreCase);
+        input = input.TrimStart(':').TrimStart(';').TrimStart();
+
+        if (DateTime.TryParseExact(input, dateFormat, provider, DateTimeStyles.AssumeUniversal, out var date))
+            return date;
+
+        if (DateTime.TryParseExact(input, dateTimeFormat, provider, DateTimeStyles.AssumeUniversal, out var dateTime))
+            return dateTime;
+
+        if (TimeSpan.TryParseExact(input, timeFormat, provider, TimeSpanStyles.None, out var time))
+            return DateTime.MinValue + time;
+
+        return null;
+    }
+}
