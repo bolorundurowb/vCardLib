59b3f4e96bc023f336c960350214ad1ade0fc86b
Bolorunduro Winner-Timothy <ogatimo@gmail.com>
2022-01-12 14:43:57 +0000

    ft: add in formatted name field tests

---
+++ vCardLib.Tests/Deserialization/FieldDeserializers/FormattedNameDeserializerTests.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using Shouldly;
+using vCardLib.Deserialization.FieldDeserializers;
+
+namespace vCardLib.Tests.Deserialization.FieldDeserializers;
+
+[TestFixture]
+public class FormattedNameDeserializerTests
+{
+    [Test]
+    public void ShouldReturnParsedValue()
+    {
+        const string input = @"FN:Mr. John Q. Public\, Esq.";
+        var deserializer = new FormattedNameDeserializer();
+        var result = deserializer.Read(input);
+
+        result.ShouldNotBeNull();
+        result.ShouldBe(@"Mr. John Q. Public\, Esq.");
+    }
+}
