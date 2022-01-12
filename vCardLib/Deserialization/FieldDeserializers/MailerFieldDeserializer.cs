e23cf9d3991c463b1da251d9b0684c8a57a76a2e
Bolorunduro Winner-Timothy <ogatimo@gmail.com>
2022-01-12 19:34:40 +0100

    ft: add in mailer deserializer

---
+++ vCardLib/Deserialization/FieldDeserializers/MailerFieldDeserializer.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.RegularExpressions;
+using vCardLib.Deserialization.Interfaces;
+
+namespace vCardLib.Deserialization.FieldDeserializers;
+
+internal class MailerFieldDeserializer : IFieldDeserializer, IV2FieldDeserializer<string>, IV3FieldDeserializer<string>,
+    IV4FieldDeserializer<string?>
+{
+    public string FieldKey => "MAILER";
+
+    public string Read(string input)
+    {
+        var separatorIndex = input.IndexOf(':');
+        return input.Substring(separatorIndex + 1).Trim();
+    }
+
+    string? IV4FieldDeserializer<string?>.Read(string input) => null;
+}
