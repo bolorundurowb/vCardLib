8e9ac002021f38bff42d7685c6d4795e094db145
Bolorunduro Winner-Timothy <ogatimo@gmail.com>
2022-01-13 00:14:49 +0100

    ft: add in title field  tests

---
+++ vCardLib.Tests/Deserialization/FieldDeserializers/TitleFieldDeserializerTests.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using Shouldly;
+using vCardLib.Deserialization.FieldDeserializers;
+
+namespace vCardLib.Tests.Deserialization.FieldDeserializers;
+
+[TestFixture]
+public class TitleFieldDeserializerTests
+{
+    [Test]
+    public void ShouldReturnParsedValue()
+    {
+        const string input = "TITLE:Research Scientist";
+        var deserializer = new TitleFieldDeserializer();
+        var result = deserializer.Read(input);
+        
+        result.ShouldNotBeNull();
+        result.ShouldBe("Research Scientist");
+    }
+}
