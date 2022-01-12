12d29c1807bd612a738e654dffcc1f3240b7cd59
Bolorunduro Winner-Timothy <ogatimo@gmail.com>
2022-01-12 19:55:44 +0100

    ft: add in mailer deserializer tests

---
+++ vCardLib.Tests/Deserialization/FieldDeserializers/MailerFieldDeserializerTests.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+using Shouldly;
+
+namespace vCardLib.Tests.Deserialization.FieldDeserializers;
+
+[TestFixture]
+public class MailerFieldDeserializerTests
+{
+    [Test]
+    public void ShouldReturnNullForV4()
+    {
+        const string input = "MAILER:PigeonMail 2.1";
+        IV4FieldDeserializer<string?> deserializer = new MailerFieldDeserializer();
+        var result = deserializer.Read(input);
+        
+        result.ShouldBeNull();
+    }
+    
+    [Test]
+    public void ShouldReturnValueForV2OrV3()
+    {
+        const string input = "MAILER:PigeonMail 2.1";
+        var deserializer = new MailerFieldDeserializer();
+        var result = deserializer.Read(input);
+        
+        result.ShouldNotBeNull();
+        result.ShouldBe("PigeonMail 2.1");
+    }
+}
