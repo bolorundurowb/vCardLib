3b0866794cc548a6b2ff6fddb43fae86b11e541c
Bolorunduro Winner-Timothy <ogatimo@gmail.com>
2022-01-13 13:48:44 +0100

    ft: add agent des tests

---
+++ vCardLib.Tests/Deserialization/FieldDeserializers/AgentFieldDeserializerTests.cs
@@ -0,0 +1,53 @@
+﻿using NUnit.Framework;
+using Shouldly;
+using vCardLib.Deserialization.FieldDeserializers;
+using vCardLib.Deserialization.Interfaces;
+
+namespace vCardLib.Tests.Deserialization.FieldDeserializers;
+
+[TestFixture]
+public class AgentFieldDeserializerTests
+{
+    [Test]
+    public void ShouldReturnNullForV4()
+    {
+        const string input = "AGENT:http://mi6.gov.uk/007";
+        IV4FieldDeserializer<string?> deserializer = new AgentFieldDeserializer();
+        var result = deserializer.Read(input);
+        
+        result.ShouldBeNull();
+    }
+    
+    [Test]
+    public void ShouldReturnValueForSimpleInput()
+    {
+        const string input = "AGENT:http://mi6.gov.uk/007";
+        var deserializer = new AgentFieldDeserializer();
+        var result = deserializer.Read(input);
+        
+        result.ShouldNotBeNull();
+        result.ShouldBe("http://mi6.gov.uk/007");
+    }
+    
+    [Test]
+    public void ShouldReturnValueForValueTypeInput()
+    {
+        const string input = "AGENT;VALUE=uri:CID:JQPUBLIC.part3.960129T083020.xyzMail@host3.com";
+        var deserializer = new AgentFieldDeserializer();
+        var result = deserializer.Read(input);
+        
+        result.ShouldNotBeNull();
+        result.ShouldBe("uri:CID:JQPUBLIC.part3.960129T083020.xyzMail@host3.com");
+    }
+    
+    [Test]
+    public void ShouldReturnValueForNestedInput()
+    {
+        const string input = @"AGENT:BEGIN:VCARD\nFN:Susan Thomas\nTEL:+1-919-555-1234\nEMAIL\;INTERNET:sthomas@host.com\nEND:VCARD\n";
+        var deserializer = new AgentFieldDeserializer();
+        var result = deserializer.Read(input);
+        
+        result.ShouldNotBeNull();
+        result.ShouldBe("BEGIN:VCARD\nFN:Susan Thomas\nTEL:+1-919-555-1234\nEMAIL;INTERNET:sthomas@host.com\nEND:VCARD\n");
+    }
+}
--- vCardLib/Deserialization/FieldDeserializers/AgentFieldDeserializer.cs
+++ vCardLib/Deserialization/FieldDeserializers/AgentFieldDeserializer.cs
@@ -1,4 +1,5 @@
-﻿using vCardLib.Deserialization.Interfaces;
+﻿using System.Text.RegularExpressions;
+using vCardLib.Deserialization.Interfaces;
 
 namespace vCardLib.Deserialization.FieldDeserializers;
 
@@ -18,7 +19,7 @@ internal class AgentFieldDeserializer : IFieldDeserializer, IV2FieldDeserializer
         if (input.StartsWith(valuePreamble))
             input = input.Replace(valuePreamble, string.Empty);
 
-        return input;
+        return Regex.Unescape(input);
     }
 
     string? IV4FieldDeserializer<string?>.Read(string input) => null;
