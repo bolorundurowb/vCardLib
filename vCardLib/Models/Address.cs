43a66f3ce576ca847ad0992e8d4e489c1db43431
Bolorunduro Winner-Timothy <ogatimo@gmail.com>
2022-01-14 10:57:51 +0100

    ft: add in address details

--- vCardLib/Models/Address.cs
+++ vCardLib/Models/Address.cs
@@ -2,5 +2,29 @@
 
 public struct Address
 {
-    
+    public string PostOfficeBox { get; set; }
+
+    public string ApartmentOrSuiteNumber { get; set; }
+
+    public string StreetAddress { get; set; }
+
+    public string CityOrLocality { get; set; }
+
+    public string StateOrProvinceOrRegion { get; set; }
+
+    public string PostalOrZipCode { get; set; }
+
+    public string Country { get; set; }
+
+    public Address(string postOfficeBox, string apartmentOrSuiteNumber, string streetAddress, string cityOrLocality,
+        string stateOrProvinceOrRegion, string postalOrZipCode, string country)
+    {
+        PostOfficeBox = postOfficeBox;
+        ApartmentOrSuiteNumber = apartmentOrSuiteNumber;
+        StreetAddress = streetAddress;
+        CityOrLocality = cityOrLocality;
+        StateOrProvinceOrRegion = stateOrProvinceOrRegion;
+        PostalOrZipCode = postalOrZipCode;
+        Country = country;
+    }
 }
