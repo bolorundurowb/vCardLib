02e49fcd58adec2ee1f9632bddd7f65450ce5f26
Bolorunduro Winner-Timothy <ogatimo@gmail.com>
2022-02-07 22:18:26 +0100

    ft: add prod id deserializer

---
+++ vCardLib/Deserialization/FieldDeserializers/ProdIdFieldDeserializer.cs
@@ -0,0 +1,17 @@
+﻿using System.Text.RegularExpressions;
+using vCardLib.Deserialization.Interfaces;
+
+namespace vCardLib.Deserialization.FieldDeserializers;
+
+internal class ProdIdFieldDeserializer : IFieldDeserializer, IV2FieldDeserializer<string>,
+    IV3FieldDeserializer<string>, IV4FieldDeserializer<string>
+{
+    public string FieldKey => "PRODID";
+
+    public string Read(string input)
+    {
+        var separatorIndex = input.IndexOf(':');
+        var value = input.Substring(separatorIndex + 1).Trim().ToLowerInvariant();
+        return Regex.Unescape(value);
+    }
+}
