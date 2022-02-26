5614fc28d3136f013831960370cdca32338267b7
Bolorunduro Winner-Timothy <ogatimo@gmail.com>
2022-02-26 19:17:46 +0100

    ft: add label model

---
+++ vCardLib/Models/Label.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using vCardLib.Enums;
+
+namespace vCardLib.Models;
+
+public struct Label
+{
+    public string Text { get; set; }
+
+    public List<AddressType> Types { get; set; } = new();
+
+    public Label(string text) => Text = text;
+}
