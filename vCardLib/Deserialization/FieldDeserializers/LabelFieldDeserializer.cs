c7ae17fd72fadbed29623b65b6b8e3ff89bf51c1
Bolorunduro Winner-Timothy <ogatimo@gmail.com>
2022-02-26 19:47:13 +0100

    ft: add label deserialization logic

--- vCardLib/Deserialization/FieldDeserializers/LabelFieldDeserializer.cs
+++ vCardLib/Deserialization/FieldDeserializers/LabelFieldDeserializer.cs
@@ -1,12 +1,17 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Text.RegularExpressions;
 using vCardLib.Deserialization.Interfaces;
+using vCardLib.Deserialization.Utilities;
 using vCardLib.Enums;
+using vCardLib.Extensions;
 using vCardLib.Models;
 
 namespace vCardLib.Deserialization.FieldDeserializers;
 
-internal class LabelFieldDeserializer : IFieldDeserializer, IV2FieldDeserializer<Label>, IV3FieldDeserializer<Label>, IV4FieldDeserializer<Label?>
+internal class LabelFieldDeserializer : IFieldDeserializer, IV2FieldDeserializer<Label>, IV3FieldDeserializer<Label>,
+    IV4FieldDeserializer<Label?>
 {
     public string FieldKey => "LABEL";
 
@@ -25,6 +30,18 @@ internal class LabelFieldDeserializer : IFieldDeserializer, IV2FieldDeserializer
 
     private static IEnumerable<AddressType> ParseTypes(string part)
     {
-        
+        const string typeKey = "TYPE=";
+        var typeGroups = part
+            .Split(';', StringSplitOptions.RemoveEmptyEntries)
+            .Select(x => x.Trim());
+
+        foreach (var typeGroup in typeGroups.Where(x => x.StartsWithIgnoreCase(typeKey)))
+        {
+            var value = typeGroup.ReplaceIgnoreCase(typeKey, string.Empty);
+            var parsedValue = SharedParsers.ParseAddressType(value);
+
+            if (parsedValue != null)
+                yield return parsedValue.Value;
+        }
     }
 }
--- vCardLib/Extensions/StringExtensions.cs
+++ vCardLib/Extensions/StringExtensions.cs
@@ -6,4 +6,10 @@ internal static class StringExtensions
 {
     public static bool EqualsIgnoreCase(this string input, string value) =>
         input.Equals(value, StringComparison.InvariantCultureIgnoreCase);
+
+    public static bool StartsWithIgnoreCase(this string input, string value) =>
+        input.StartsWith(value, StringComparison.InvariantCultureIgnoreCase);
+
+    public static string ReplaceIgnoreCase(this string input, string oldValue, string newValue) =>
+        input.Replace(oldValue, newValue, StringComparison.InvariantCultureIgnoreCase);
 }
