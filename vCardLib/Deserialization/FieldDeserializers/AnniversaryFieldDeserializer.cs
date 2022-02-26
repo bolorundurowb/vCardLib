9633980b9d22dd00095c343dced2218fc4507ab2
Bolorunduro Winner-Timothy <ogatimo@gmail.com>
2022-02-26 16:11:01 +0100

    ft: add anniversary field deserialzer

---
+++ vCardLib/Deserialization/FieldDeserializers/AnniversaryFieldDeserializer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+using vCardLib.Deserialization.Interfaces;
+using vCardLib.Deserialization.Utilities;
+
+namespace vCardLib.Deserialization.FieldDeserializers;
+
+internal class AnniversaryFieldDeserializer : IFieldDeserializer, IV2FieldDeserializer<DateTime?>,
+    IV3FieldDeserializer<DateTime?>, IV4FieldDeserializer<DateTime?>
+{
+    public string FieldKey => "ANNIVERSARY";
+
+    public DateTime? Read(string input) => null;
+
+    DateTime? IV4FieldDeserializer<DateTime?>.Read(string input)
+    {
+        input = input.ToUpper().Replace(FieldKey, string.Empty);
+        return SharedDeserializers.ParseDate(input);
+    }
+}
