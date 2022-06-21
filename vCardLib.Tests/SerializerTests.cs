1a7d2fe9b15033640a88e45aaa0883f7b7f0e4ca
Mauro Bertoli <mauro.bertoli@inmeta.no>
2022-06-21 10:38:42 +0200

    fix newline in unit tests to run tests in unix env

--- vCardLib.Tests/SerializerTests.cs
+++ vCardLib.Tests/SerializerTests.cs
@@ -70,22 +70,23 @@ namespace vCardLib.Tests
             };
             var data = Serializer.Serialize(cards);
 
-            Assert.AreEqual(data, @"BEGIN:VCARD
-VERSION:3.0
-REV:20220101T000000Z
-N:;;;;
-FN:Card1
-KIND:Individual
-GENDER:None
-END:VCARD
-BEGIN:VCARD
-VERSION:3.0
-REV:20220101T000000Z
-N:;;;;
-FN:Card2
-KIND:Individual
-GENDER:None
-END:VCARD");
+            // we need to use Environment.NewLine to pass unit-tests also in Unix environments. VS hardcodes \n\r due to Windows newline.
+            Assert.AreEqual(data, "BEGIN:VCARD" + Environment.NewLine +
+                                  "VERSION:3.0" + Environment.NewLine +
+                                  "REV:20220101T000000Z" + Environment.NewLine +
+                                  "N:;;;;" + Environment.NewLine +
+                                  "FN:Card1" + Environment.NewLine +
+                                  "KIND:Individual" + Environment.NewLine +
+                                  "GENDER:None" + Environment.NewLine +
+                                  "END:VCARD" + Environment.NewLine +
+                                  "BEGIN:VCARD" + Environment.NewLine +
+                                  "VERSION:3.0" + Environment.NewLine +
+                                  "REV:20220101T000000Z" + Environment.NewLine +
+                                  "N:;;;;" + Environment.NewLine +
+                                  "FN:Card2" + Environment.NewLine +
+                                  "KIND:Individual" + Environment.NewLine +
+                                  "GENDER:None" + Environment.NewLine +
+                                  "END:VCARD");
         }
 
         [Test]
