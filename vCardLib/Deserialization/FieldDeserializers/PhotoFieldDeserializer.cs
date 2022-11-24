bcf7e8da9d457e2836ac506bc918f8b807485899
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2022-11-24 22:58:11 +0100

    ft: add photo deserializer scaffold

---
+++ vCardLib/Deserialization/FieldDeserializers/PhotoFieldDeserializer.cs
@@ -0,0 +1,15 @@
+﻿using vCardLib.Deserialization.Interfaces;
+using vCardLib.Models;
+
+namespace vCardLib.Deserialization.FieldDeserializers;
+
+internal sealed class PhotoFieldDeserializer : IFieldDeserializer, IV2FieldDeserializer<Photo>, IV3FieldDeserializer<Photo>, IV4FieldDeserializer<Photo>
+{
+    public string FieldKey => "PHOTO";
+
+    Photo IV2FieldDeserializer<Photo>.Read(string input) => throw new System.NotImplementedException();
+    
+    Photo IV3FieldDeserializer<Photo>.Read(string input) => throw new System.NotImplementedException();
+    
+    Photo IV4FieldDeserializer<Photo>.Read(string input) => throw new System.NotImplementedException();
+}
