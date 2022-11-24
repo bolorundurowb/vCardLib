508de88616aac8250ae14e5a5ad2a8d9c933517e
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2022-11-24 22:57:55 +0100

    ft: add photo model updates

--- vCardLib/Models/Photo.cs
+++ vCardLib/Models/Photo.cs
@@ -3,43 +3,24 @@ using vCardLib.Enums;
 
 namespace vCardLib.Models;
 
-/// <summary>
-/// Class to hold images embedded in the vCard 
-/// </summary>
-public class Photo
+public struct Photo
 {
     /// <summary>
     /// The image data
     /// </summary>
-    public byte[] Picture { get; set; }
+    public string? Type { get; set; }
 
     /// <summary>
     /// The image type
     /// </summary>
-    public PhotoType Type { get; set; }
+    public string? MimeType { get; set; }
 
     /// <summary>
     /// The encoding of the image
     /// </summary>
-    public PhotoEncoding Encoding { get; set; }
+    public string? Encoding { get; set; }
 
-    /// <summary>
-    /// The URL for remote images
-    /// </summary>
-    public string PhotoURL { get; set; }
+    public string Value { get; set; }
 
-    /// <summary>
-    /// Converts the embedded image to a base 64 string
-    /// </summary>
-    /// <returns>An empty string  if the picture is null or a base 64 representation of the image</returns>
-    public string ToBase64String()
-    {
-        if (Picture == null)
-        {
-            return "";
-        }
-
-        var base64String = Convert.ToBase64String(Picture);
-        return base64String;
-    }
+    public byte[] AsByteArray() => Convert.FromBase64String(Value);
 }
