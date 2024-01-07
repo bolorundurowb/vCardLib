b653f92b89f5f05ec156f1344735e17e7c2b263e
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2024-01-07 03:33:51 +0100

    ft: add birthday field serializer

---
+++ vCardLib/Serialization/FieldSerializers/BirthdayFieldSerializer.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using vCardLib.Serialization.Interfaces;
+
+namespace vCardLib.Serialization.FieldSerializers;
+
+internal sealed class BirthdayFieldSerializer : IV2FieldSerializer<DateTime>, IV3FieldSerializer<DateTime>, IV4FieldSerializer<DateTime>
+{
+    public string FieldKey => "BDAY";
+    
+    public string? Write(DateTime data) => $"{FieldKey}: {data:yyyyMMdd}";
+}
