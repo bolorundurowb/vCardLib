36b901a5d099bbb6b9daa40b811e6b429d8f868c
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2024-01-07 20:24:53 +0100

    ft: add categories field serializer

---
+++ vCardLib/Serialization/FieldSerializers/CategoriesFieldSerializer.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using vCardLib.Serialization.Interfaces;
+
+namespace vCardLib.Serialization.FieldSerializers;
+
+internal sealed class CategoriesFieldSerializer : IV2FieldSerializer<List<string>>, IV3FieldSerializer<List<string>>,
+    IV4FieldSerializer<List<string>>
+{
+    public string FieldKey => "CATEGORIES";
+
+    public string? Write(List<string> data)
+    {
+        var value = string.Join(",", data);
+        return $"{FieldKey}: {value}";
+    }
+}
