f6d7187401aad07f5c2b94e19dce21760e8d2f2e
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2024-02-12 13:24:45 +0100

    ft: finalize serialization ov erride

--- vCardLib/Serialization/vCardSerializer.cs
+++ vCardLib/Serialization/vCardSerializer.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using vCardLib.Enums;
 using vCardLib.Models;
 using vCardLib.Serialization.FieldSerializers;
@@ -52,15 +53,55 @@ public static class vCardSerializer
     {
         var version = overrideVersion ?? card.Version;
 
-        if (version is vCardVersion.v2) 
+        if (version is vCardVersion.v2)
             return (new V2Serializer(FieldSerializers)).Serialize(card);
 
-        if (version is vCardVersion.v3) 
+        if (version is vCardVersion.v3)
             return (new V3Serializer(FieldSerializers)).Serialize(card);
 
-        if (version is vCardVersion.v4) 
+        if (version is vCardVersion.v4)
             return (new V4Serializer(FieldSerializers)).Serialize(card);
 
         throw new ArgumentException("Unknown version", nameof(version));
+    }
+
+    public static string Serialize(IEnumerable<vCard> cards, vCardVersion? overrideVersion = null)
+    {
+        var cardList = cards.ToList();
+
+        if (cardList.Count == 0)
+            return string.Empty;
+
+        var version = overrideVersion ?? cardList.First().Version;
+        var builder = new StringBuilder();
+
+        switch (version)
+        {
+            case vCardVersion.v2:
+            {
+                var serializer = new V2Serializer(FieldSerializers);
+                foreach (var card in cardList)
+                    builder.AppendLine(serializer.Serialize(card));
+                break;
+            }
+            case vCardVersion.v3:
+            {
+                var serializer = new V3Serializer(FieldSerializers);
+                foreach (var card in cardList)
+                    builder.AppendLine(serializer.Serialize(card));
+                break;
+            }
+            case vCardVersion.v4:
+            {
+                var serializer = new V4Serializer(FieldSerializers);
+                foreach (var card in cardList)
+                    builder.AppendLine(serializer.Serialize(card));
+                break;
+            }
+            default:
+                throw new ArgumentException("Unknown version", nameof(version));
+        }
+
+        return builder.ToString();
     }
 }
