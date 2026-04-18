2ccf5ae31452fa0d1de4a5be401276cd92e1553f
Winner-Timothy Bolorunduro <bolorundurowb@gmail.com>
2026-04-18 11:00:04 +0100

    ft: add decomposers for complex vCard type flag handling
    - Introduced `DecomposeAddressTypes` method to extract individual address types from flags.
    - Added `DecomposeEmailAddressTypes` method to break down email address flags into components.
    - Implemented `DecomposeTelephoneNumberTypes` to handle telephone type flag decomposition.
    - Enhanced utility handling for `AddressType`, `EmailAddressType`, and `TelephoneNumberType` enums, improving RFC compliance.

--- vCardLib/Serialization/Utilities/SharedDecomposers.cs
+++ vCardLib/Serialization/Utilities/SharedDecomposers.cs
@@ -1,10 +1,20 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using vCardLib.Enums;
 
 namespace vCardLib.Serialization.Utilities;
 
 internal static class SharedDecomposers
 {
+    public static IEnumerable<string> DecomposeAddressTypes(this AddressType input)
+    {
+        return Enum.GetValues(typeof(AddressType))
+            .Cast<AddressType>()
+            .Where(x => input.HasFlag(x) && x != AddressType.None)
+            .Select(x => x.DecomposeAddressType());
+    }
+
     public static string DecomposeAddressType(this AddressType input)
     {
         return input switch
@@ -17,6 +27,14 @@ internal static class SharedDecomposers
             AddressType.Work => "work",
             _ => throw new ArgumentException()
         };
+    }
+
+    public static IEnumerable<string> DecomposeEmailAddressTypes(this EmailAddressType input)
+    {
+        return Enum.GetValues(typeof(EmailAddressType))
+            .Cast<EmailAddressType>()
+            .Where(x => input.HasFlag(x) && x != EmailAddressType.None)
+            .Select(x => x.DecomposeEmailAddressType());
     }
 
     public static string DecomposeEmailAddressType(this EmailAddressType input)
@@ -57,6 +75,14 @@ internal static class SharedDecomposers
             ContactKind.Location => "location",
             _ => throw new ArgumentException()
         };
+    }
+
+    public static IEnumerable<string> DecomposeTelephoneNumberTypes(this TelephoneNumberType input)
+    {
+        return Enum.GetValues(typeof(TelephoneNumberType))
+            .Cast<TelephoneNumberType>()
+            .Where(x => input.HasFlag(x) && x != TelephoneNumberType.None)
+            .Select(x => x.DecomposeTelephoneNumberType());
     }
 
     public static string DecomposeTelephoneNumberType(this TelephoneNumberType input)
